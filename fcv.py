03f36c56c06b896088f18a2cf01fbc49977306a3
Ajay Arjun <arjun.bka@gmail.com>
2016-09-14 23:01:21 -0500

    k fold factoring updated return set

--- fcv.py
+++ fcv.py
@@ -3,7 +3,7 @@ import random
 
 import pandas as pd
 
-'''##################################       Loading Data      ########################################'''
+##################################       Loading Data      ########################################
 attributes = pd.read_csv('ecoli.csv', usecols=[0, 1, 2, 3, 4, 5, 6])
 # print attributes
 attribute_name_list = list(attributes.columns.values)
@@ -11,36 +11,33 @@ attribute_name_list = list(attributes.columns.values)
 class_val = pd.read_csv('ecoli.csv', usecols=[7])
 # print class_val
 class_name_list = list(class_val.columns.values)
+
+
 # print class_name_list
-'''##################################      Normalizing Data       ########################################'''
-parts = []
+
+##################################      Normalizing Data       ########################################
 
 
 def normalization():
-    for i in attribute_name_list:
-        print i
-        max_attribute_val = attributes[i].max()
-        print "max_attribute_val is : ", max_attribute_val
-        min_attribute_val = attributes[i].min()
-        print "min is : ", min_attribute_val
-        for index, row in attributes.iterrows():
-            # print "row val ",row[i]
-            if min_attribute_val < 0 or max_attribute_val > 1:
-                # if row[i] < 0 or row[i] > 1:
-                # print "inside if loop"
-                # print "row val before normalization ", row[i]
-                normalized_value = round((row[i] - min_attribute_val) / (max_attribute_val - min_attribute_val), 2)
-                # print "normalized value is ",normalized_value
+    choice = raw_input("Do you want to normalize data")
+    choice = choice.lower()
+    if choice == 'y':
+        for i in attribute_name_list:
+            print i
+            max_attribute_val = attributes[i].max()
+            print "max_attribute_val is : ", max_attribute_val
+            min_attribute_val = attributes[i].min()
+            print "min is : ", min_attribute_val
+            for index, row in attributes.iterrows():
+                normalized_value = round((row[i] - min_attribute_val) / (max_attribute_val - min_attribute_val), 3)
                 row[i] = normalized_value
-                # print "row val after normalization ", row[i]
-                # print attributes
-                # print attribute_name_list
-                # getch = raw_input("getch")
-    print attributes
-    # attributes.to_csv("norm.csv")
+        print attributes
+        # attributes.to_csv("norm.csv")
+    else:
+        print "Proceeeding without Normalization"
 
 
-'''##################################     Patitioning Data   ########################################'''
+##################################     Patitioning Data   ########################################
 
 
 def partition():
@@ -48,43 +45,48 @@ def partition():
     n = int(n)
     shape = attributes.shape
     no_of_rows = shape[0]
-    # print no_of_rows
     row_per_part = float(no_of_rows) / float(n)
     row_per_part = math.ceil(row_per_part)
     print "rows per partition is ", row_per_part
     index_list = range(0, no_of_rows)
     random.shuffle(index_list)
-    # print index_list
     attributes['part'] = int(0)
     class_val['part1'] = int(0)
     for i in range(1, n + 1):
-        # print "printing value of i = ", i
         count = 1
         while count <= row_per_part:
             if len(index_list) != 0:
-                # print "Index list before choice \n", index_list
                 num = random.choice(index_list)
-                # print "selected num is : ", num
                 index_list.remove(num)
-                # print "Index list after  choice \n", index_list
                 attributes.iloc[num, 7] = i
                 class_val.iloc[num, 1] = i
-                # print attributes.iloc[num, 7]
-                # getch = raw_input("getch")
             count += 1
-    # print attributes
-    # print class_val
-    # print class_val.groupby('part1').count()
     merged_data = pd.concat([attributes, class_val], axis=1)
     merged_data.drop('part1', axis=1, inplace=True)
-    print merged_data
     part_list = merged_data.part.unique()
-    # match_results.stadium.unique()
+    parts = []
     part_list = sorted(part_list)
     for item in part_list:
         temp_df = merged_data[merged_data['part'] == int(item)]
         parts.append(temp_df)
     print parts
+    final_data = parts
+    print type(final_data)
+    print len(parts)
+    print len(final_data)
+    print "train"
+
+    for item in final_data:
+        test_data = []
+        chosen_part = int(item.part.unique())
+        test_data.append(item)
+        print "traiing data\n"
+        for j in final_data:
+            if not(int(j.part.unique()) == chosen_part):
+                print j
+        print "TEST DATA \n"+"#"*100
+        print test_data
+        x = raw_input("wait")
 
 
 def return_partition():
@@ -95,11 +97,23 @@ def return_partition():
     print temp_df
 
 
-def train():
-    print "train"
+# def train(parts):
+#     print "inside train"
+#     final_data = parts
+#     print final_data
+#     for i in final_data:
+#         print "inside for loop\n"
+#         parts = final_data
+#         test_data = parts[i]
+#         parts.remove(i)
+#         # print "training with\n"
+#         # print parts+"\n\n"
+#         print "testing with"+"\n\n"
+#         print test_data+"\n\n"
+#     print "done"
 
 
 normalization()
 partition()
 return_partition()
-train()
+
